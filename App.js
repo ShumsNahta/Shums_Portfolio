90ec0c0a097eea02ef7156beb0c2fc758dd40932
Shums Nahta <148189159+ShumsNahta@users.noreply.github.com>
2024-04-20 18:01:50 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,23 @@
+import "./App.css"
+import { BrowserRouter } from "react-router-dom";
+import Navbar1 from "./Components/Navbar";
+import Intro from "./Components/Intro";
+import Skills from "./Components/Skills";
+import Portfolio from "./Components/Portfolio";
+import Contact from "./Components/Contact";
+
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Navbar1 />
+        <Intro />
+        <Skills />
+        <Portfolio />
+        <Contact />
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
